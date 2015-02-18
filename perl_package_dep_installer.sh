d24712c58e5fd8e820989c01a3f3cce785363b64
Sawood Alam <ibnesayeed@gmail.com>
2015-02-18 17:19:20 -0500

    Perl package dependency installation separated from the main INSTALL script

---
+++ perl_package_dep_installer.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+perl -MCPAN -e 'install HTML::TagParser'
+perl -MCPAN -e 'install HTML::LinkExtractor'
+perl -MCPAN -e 'install HTTP::Cookies'
+perl -MCPAN -e 'install HTTP::Status'
+perl -MCPAN -e 'install URI'
+perl -MCPAN -e 'install CSS'
+perl -MCPAN -e 'install HTTP::Date'
+perl -MCPAN -e 'install Getopt::Long'
